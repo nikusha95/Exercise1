9d7836bba72d665e2cc5425a82ee3db010708229
nikusha95 <nikasanaia11@yahoo.com>
2022-09-24 11:44:09 +0400

    init

---
+++ Exercise1/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Threading.Channels;
+
+string s = Console.ReadLine();
+
+bool isInt = int.TryParse(s, out int result);
+
+//int y = int.Parse(s);
+if (isInt)
+{
+    Console.WriteLine(result);
+}
+else
+{
+    Console.WriteLine("no number");
+}
+
+
+int x = 10, y = 20;
+
+(x,y)=(y,x);
+// int tmp = x;
+//
+// x = y;
+// y = tmp;
+
+Console.WriteLine($"{x} {y}");
